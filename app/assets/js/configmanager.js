4e5138aa70fc2bb27b797313763cc8f375d1cafd
alio13013 <aliobr.acp@gmail.com>
2025-06-21 16:20:26 -0300

    .aurora

--- app/assets/js/configmanager.js
+++ app/assets/js/configmanager.js
@@ -9,7 +9,7 @@ const logger = LoggerUtil.getLogger('ConfigManager')
 
 const sysRoot = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME)
 
-const dataPath = path.join(sysRoot, '.HastaStudiosLauncher')
+const dataPath = path.join(sysRoot, '.AuroraVitaeLauncher')
 
 const launcherDir = app.getPath('userData')
 
